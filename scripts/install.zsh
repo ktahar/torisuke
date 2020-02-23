e1ced7e2c1b5291600b7661dd08024d167b2a5f8
Kosuke Tahara <ksk.tahara@gmail.com>
2020-02-23 15:11:00 +0900

    setup colcon-argcomplete

--- scripts/install.zsh
+++ scripts/install.zsh
@@ -9,3 +9,10 @@ if ! dpkg-query -W ${pkgs[@]} &> /dev/null; then
 else
     echo "All required apt packages are installed"
 fi
+
+col="source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh"
+if ! grep $col ~/.zshrc &> /dev/null; then
+    echo $col >> ~/.zshrc
+else
+    echo "colcon-argcomplete has been set up already."
+fi
